8ff29ccde5e1387eb58af695cd3b293e1529f64c
hihaluemen <1596916766@qq.com>
2024-11-02 01:09:20 +0800

    更新新的画法，和测试1101

--- post_json.py
+++ post_json.py
@@ -47,10 +47,136 @@ url = 'http://127.0.0.1:29000/data-new'
 #                   ]
 # }
 
+# payload = {
+#   "work_data": [
+#     {
+#       "identifier": "2",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "施工计划修正",
+#       "duration": "4",
+#       "prerequisite": "",
+#       "start_date": "2006-01-01",
+#       "end_date": "2006-01-05",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "7",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "1号楼施工准备",
+#       "duration": "6",
+#       "prerequisite": "2",
+#       "start_date": "2006-01-05",
+#       "end_date": "2006-01-11",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "8",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "1号楼机电工程安装",
+#       "duration": "26",
+#       "prerequisite": "7",
+#       "start_date": "2006-01-11",
+#       "end_date": "2006-02-06",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "11",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "2号楼施工准备",
+#       "duration": "6",
+#       "prerequisite": "2",
+#       "start_date": "2006-01-05",
+#       "end_date": "2006-01-11",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "12",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "2号楼机电工程安装",
+#       "duration": "39",
+#       "prerequisite": "11",
+#       "start_date": "2006-01-11",
+#       "end_date": "2006-02-19",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "13",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "2号楼分项工程检验、调试",
+#       "duration": "14",
+#       "prerequisite": "12",
+#       "start_date": "2006-02-19",
+#       "end_date": "2006-03-05",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "16",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "3号楼施工准备",
+#       "duration": "6",
+#       "prerequisite": "2",
+#       "start_date": "2006-01-05",
+#       "end_date": "2006-01-11",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "17",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "3号楼机电工程安装",
+#       "duration": "18",
+#       "prerequisite": "16",
+#       "start_date": "2006-01-11",
+#       "end_date": "2006-01-29",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "18",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "3号楼分项工程检验、调试",
+#       "duration": "8",
+#       "prerequisite": "17",
+#       "start_date": "2006-01-29",
+#       "end_date": "2006-02-06",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "20",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "全系统联调、试运行",
+#       "duration": "10",
+#       "prerequisite": "8,13,18",
+#       "start_date": "2006-03-05",
+#       "end_date": "2006-03-15",
+#       "cost": ""
+#     },
+#     {
+#       "identifier": "21",
+#       "partition": "施工",
+#       "classify": "施工",
+#       "name": "综合交工验收",
+#       "duration": "5",
+#       "prerequisite": "20",
+#       "start_date": "2006-03-15",
+#       "end_date": "2006-03-20",
+#       "cost": ""
+#     }
+#   ]
+# }
+
 payload = {
   "work_data": [
     {
-      "identifier": "2",
+      "identifier": "A",
       "partition": "施工",
       "classify": "施工",
       "name": "施工计划修正",
@@ -61,111 +187,111 @@ payload = {
       "cost": ""
     },
     {
-      "identifier": "7",
+      "identifier": "B",
       "partition": "施工",
       "classify": "施工",
       "name": "1号楼施工准备",
       "duration": "6",
-      "prerequisite": "2",
+      "prerequisite": "A",
       "start_date": "2006-01-05",
       "end_date": "2006-01-11",
       "cost": ""
     },
     {
-      "identifier": "8",
+      "identifier": "C",
       "partition": "施工",
       "classify": "施工",
       "name": "1号楼机电工程安装",
       "duration": "26",
-      "prerequisite": "7",
+      "prerequisite": "B",
       "start_date": "2006-01-11",
       "end_date": "2006-02-06",
       "cost": ""
     },
     {
-      "identifier": "11",
+      "identifier": "D",
       "partition": "施工",
       "classify": "施工",
       "name": "2号楼施工准备",
       "duration": "6",
-      "prerequisite": "2",
+      "prerequisite": "A",
       "start_date": "2006-01-05",
       "end_date": "2006-01-11",
       "cost": ""
     },
     {
-      "identifier": "12",
+      "identifier": "E",
       "partition": "施工",
       "classify": "施工",
       "name": "2号楼机电工程安装",
       "duration": "39",
-      "prerequisite": "11",
+      "prerequisite": "D",
       "start_date": "2006-01-11",
       "end_date": "2006-02-19",
       "cost": ""
     },
     {
-      "identifier": "13",
+      "identifier": "F",
       "partition": "施工",
       "classify": "施工",
       "name": "2号楼分项工程检验、调试",
       "duration": "14",
-      "prerequisite": "12",
+      "prerequisite": "E",
       "start_date": "2006-02-19",
       "end_date": "2006-03-05",
       "cost": ""
     },
     {
-      "identifier": "16",
+      "identifier": "G",
       "partition": "施工",
       "classify": "施工",
       "name": "3号楼施工准备",
       "duration": "6",
-      "prerequisite": "2",
+      "prerequisite": "A",
       "start_date": "2006-01-05",
       "end_date": "2006-01-11",
       "cost": ""
     },
     {
-      "identifier": "17",
+      "identifier": "H",
       "partition": "施工",
       "classify": "施工",
       "name": "3号楼机电工程安装",
       "duration": "18",
-      "prerequisite": "16",
+      "prerequisite": "G",
       "start_date": "2006-01-11",
       "end_date": "2006-01-29",
       "cost": ""
     },
     {
-      "identifier": "18",
+      "identifier": "I",
       "partition": "施工",
       "classify": "施工",
       "name": "3号楼分项工程检验、调试",
       "duration": "8",
-      "prerequisite": "17",
+      "prerequisite": "H",
       "start_date": "2006-01-29",
       "end_date": "2006-02-06",
       "cost": ""
     },
     {
-      "identifier": "20",
+      "identifier": "J",
       "partition": "施工",
       "classify": "施工",
       "name": "全系统联调、试运行",
       "duration": "10",
-      "prerequisite": "8,13,18",
+      "prerequisite": "C,F,I",
       "start_date": "2006-03-05",
       "end_date": "2006-03-15",
       "cost": ""
     },
     {
-      "identifier": "21",
+      "identifier": "K",
       "partition": "施工",
       "classify": "施工",
       "name": "综合交工验收",
       "duration": "5",
-      "prerequisite": "20",
+      "prerequisite": "J",
       "start_date": "2006-03-15",
       "end_date": "2006-03-20",
       "cost": ""
@@ -173,6 +299,76 @@ payload = {
   ]
 }
 
+payload = {
+  "work_data": [
+  {
+    "identifier": "A",
+    "partition": "土建工程",
+    "classify": "基础施工",
+    "name": "基础开挖",
+    "duration": 5,
+    "prerequisite": "",
+    "start_date": "2023-04-01",
+    "end_date": "2023-04-06",
+    "cost": ""
+  },
+  {
+    "identifier": "B",
+    "partition": "土建工程",
+    "classify": "主体结构",
+    "name": "混凝土浇筑",
+    "duration": 7,
+    "prerequisite": "A",
+    "start_date": "2023-04-06",
+    "end_date": "2023-04-13",
+    "cost": ""
+  },
+  {
+    "identifier": "C",
+    "partition": "电气工程",
+    "classify": "电缆敷设",
+    "name": "主电缆铺设",
+    "duration": 7,
+    "prerequisite": "A",
+    "start_date": "2023-04-06",
+    "end_date": "2023-04-13",
+    "cost": ""
+  },
+  {
+    "identifier": "D",
+    "partition": "电气工程",
+    "classify": "设备安装",
+    "name": "变压器安装",
+    "duration": 2,
+    "prerequisite": "B",
+    "start_date": "2023-04-13",
+    "end_date": "2023-04-15",
+    "cost": ""
+  },
+  {
+    "identifier": "E",
+    "partition": "暖通空调工程",
+    "classify": "管道安装",
+    "name": "冷热水管路安装",
+    "duration": 4,
+    "prerequisite": "B,C",
+    "start_date": "2023-04-13",
+    "end_date": "2023-04-17",
+    "cost": ""
+  },
+  {
+    "identifier": "F",
+    "partition": "暖通空调工程",
+    "classify": "设备安装",
+    "name": "空调机组安装",
+    "duration": 3,
+    "prerequisite": "D,E",
+    "start_date": "2023-04-17",
+    "end_date": "2023-04-19",
+    "cost": ""
+  }
+]
+}
 
 
 
@@ -184,3 +380,9 @@ response = requests.post(url, data=json_data, headers={'Content-Type': 'applicat
 
 # 打印响应内容
 print(response.text)
+for k, v in response.json().items():
+  if k == 'edge_info':
+    for kk, vv in v.items():
+      print(kk)
+      print(vv)
+      print('=======')
